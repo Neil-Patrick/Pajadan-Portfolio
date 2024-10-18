2c9795e3995e09739985fc8e98e94d18f37db5dd
Neil Patrick Pajadan <152168399+Neil-Patrick@users.noreply.github.com>
2024-10-18 19:41:00 +0800

    Update website.js

--- website.js
+++ website.js
@@ -1,15 +1,16 @@
-function openModal() {
-    var modal = document.getElementById("imageModal");
-    var fullImage = document.getElementById("fullImage");
-    
-    fullImage.src = document.querySelector(".profile-pic").src;
-    
-    modal.style.display = "block";
+const typewriterText = document.getElementById('typewriter-text');
+const textToType = 'Greetings, I am Neil Patrick Pajadan';
+let charIndex = 0;
+
+function typeWriter() {
+  if (charIndex < textToType.length) {
+    typewriterText.textContent += textToType.charAt(charIndex);
+    charIndex++;
+    setTimeout(typeWriter, 75); 
+  }
 }
 
-function closeModal() {
-    document.getElementById("imageModal").style.display = "none";
-}
+typeWriter();
 
 document.getElementById("theme-toggle").addEventListener("click", function() {
     const body = document.body;
@@ -25,4 +26,25 @@ document.getElementById("theme-toggle").addEventListener("click", function() {
         moonIcon.style.display = "inline"; 
         sunIcon.style.display = "none"; 
     }
+});
+
+function openModal() {
+    var modal = document.getElementById("imageModal");
+    var fullImage = document.getElementById("fullImage");
+    
+    fullImage.src = document.querySelector(".profile-pic").src;
+    
+    modal.style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("imageModal").style.display = "none";
+}
+
+const mobileMenu = document.getElementById('mobile-menu');
+const navList = document.querySelector('.nav-elements');
+
+mobileMenu.addEventListener('click', () => {
+    navList.classList.toggle('active'); 
+    mobileMenu.classList.toggle('active');
 });
